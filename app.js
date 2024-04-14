f5ad1003e8b97f6ff1279cc11f7ccff41615ed43
YasminMell0 <yasmin.carvalho.mell00@gmail.com>
2024-04-14 18:10:37 -0300

    Second commit

--- app.js
+++ app.js
@@ -1,50 +1,82 @@
 const express = require("express")
 const app = express()
+const handlebars = require("express-handlebars").engine
+const hand = require("handlebars")
+const bodyParser = require("body-parser")
+const post = require("./models/post")
 
-const Sequelize = require("sequelize")
-const sequelize = new Sequelize("exemplo", "root", "",{
-    host: "localhost",
-    dialect: "mysql"
+hand.registerHelper("eq", function(a,b){
+    return a === b;
 })
 
-sequelize.authenticate().then(function(){
-    console.log("Conectado com sucesso!")
-}).catch(function(erro){
-    console.log("Erro ao conectar: " + erro)
+app.engine("handlebars", handlebars({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get("/", function(req,res){
+    res.render("primeira_pagina")
 })
 
-//criando tabela
-const Agendamentos = sequelize.define("agendamentos",{
-    nome:{
-        type: Sequelize.STRING
-    },
-    bairro:{
-        type: Sequelize.STRING
-    }
+app.get("/pagcadastro", function(req,res){
+    res.render("pagcadastrar")
 })
 
-//comentar depois de rodar a primeira vez
-//Agendamentos.sync({force: true})
-
-//ligado o servidor
-app.listen(8081, function(){
-    console.log("Servidor ativo")
-})
-
-//fazendo rotas
-app.get("/", function(req, res){
-    console.log("Página principal")
-    res.send("Primeira página")
-})
-
-app.get("/cadastrar", async function(req, res) {
-    console.log("Página cadastrada")
-    res.send("Primeira cadastrada")
-    
-    // Cria um novo registro na tabela 'Agendamentos'
-    Agendamentos.create({
-        nome: "Teste2",
-        bairro: "testinho"
+app.post("/cadastrar", function(req,res){
+    post.create({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    }).then(function(){
+        res.render("pagcadastrar")
+    }).catch(function(erro){
+        res.send("Falha ao cadastrar os dados: " +erro)
     })
-  });
-  
+})
+
+app.get("/consulta", function(req, res){
+    post.findAll().then(function(post){
+        res.render("consulta", {post})
+    }).catch(function(erro){
+        console.log("Erro ao carregar dados do banco: " + erro)
+    })
+})
+
+app.get("/editar/:id", function(req, res){
+    post.findAll({where: {'id': req.params.id}}).then(function(post){ 
+        res.render("editar", { post });
+    }).catch(function(erro){
+        console.log("Erro ao carregar dados do banco: " + erro)
+    })
+})
+
+app.get("/excluir/:id", function(req, res){
+    post.destroy({where: {'id': req.params.id}}).then(function(){
+        res.render("primeira_pagina")
+    }).catch(function(erro){
+        console.log("Erro ao excluir ou encontrar os dados do banco: " + erro)
+    })
+})
+
+app.post("/atualizar", function(req, res){
+    post.update({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    },{
+        where: {
+            id: req.body.id
+        }
+    }).then(function(){
+        res.redirect("/consulta")
+    })
+})
+
+app.listen(8081, function(){
+    console.log("Servidor Ativo")
+})
